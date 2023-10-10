2fe7c45c789a14b0948c778aef4246cc29993ca5
dotnetsharpist <sheyxcontacts@gmail.com>
2023-10-11 00:59:27 +0500

    UnitOfWork

--- Authentication.DataAccess/Repositories/Repository.cs
+++ Authentication.DataAccess/Repositories/Repository.cs
@@ -1,12 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Authentication.DataAccess.Contexts;
+using Authentication.DataAccess.IRepositories;
+using Authentication.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq.Expressions;
 
-namespace Authentication.DataAccess.Repositories
+namespace Authentication.DataAccess.Repositories;
+
+public class Repository<T> : IRepository<T> where T : Auditable
 {
-    internal class Repository
+    private readonly DbSet<T> table;
+    private readonly AuthenticationDbContext dbContext;
+
+    public Repository(AuthenticationDbContext dbContext)
     {
+        this.dbContext = dbContext;
+        table = dbContext.Set<T>();
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await table.AddAsync(entity);
+
+        return entity;
+    }
+
+    public async Task<T> UpdateAsync(T entity)
+    {
+        EntityEntry<T> entry = this.dbContext.Update(entity);
+
+        return entry.Entity;
+    }
+
+    public async Task<bool> DeleteAsync(Expression<Func<T, bool>> expression)
+    {
+        var entity = await this.SelectAsync(expression);
+
+        if (entity is not null)
+        {
+            entity.IsDeleted = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    public async Task<T> SelectAsync(Expression<Func<T, bool>> expression, string[] includes = null)
+        => await this.SelectAll(expression, includes).FirstOrDefaultAsync(t => !t.IsDeleted);
+
+    public IQueryable<T> SelectAll(Expression<Func<T, bool>> expression = null, string[] includes = null, bool isTracking = true)
+    {
+        var query = expression is null ? isTracking ? table : table.AsNoTracking()
+            : isTracking ? table.Where(expression) : table.Where(expression).AsNoTracking();
+
+        query = query.Where(t => !t.IsDeleted);
+
+        if (includes is not null)
+            foreach (var include in includes)
+                query = query.Include(include);
+
+        return query;
+    }
+
+    public async Task SaveAsync()
+    {
+        await dbContext.SaveChangesAsync();
     }
 }
--- Authentication.DataAccess/Repositories/UnitOfWork.cs
+++ Authentication.DataAccess/Repositories/UnitOfWork.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Authentication.DataAccess.Contexts;
+using Authentication.DataAccess.IRepositories;
+using Authentication.Domain.Entities.User;
 
-namespace Authentication.DataAccess.Repositories
+namespace Authentication.DataAccess.Repositories;
+
+public class UnitOfWork
 {
-    internal class UnitOfWork
+    private readonly AuthenticationDbContext dbContext;
+
+    public UnitOfWork(AuthenticationDbContext dbContext)
     {
+        this.dbContext = dbContext;
+        UserRepository = new Repository<User>(dbContext);
+    }
+
+    public IRepository<User> UserRepository { get; set; }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(true);
+    }
+
+    public async Task<bool> SaveAsync()
+    {
+        var saved = await this.dbContext.SaveChangesAsync();
+        return saved > 0;
     }
 }
