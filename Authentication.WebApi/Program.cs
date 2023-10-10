194d551af1a0dd9a8375b87be95c4f6f7fc0e6dc
dotnetsharpist <sheyxcontacts@gmail.com>
2023-10-11 02:25:50 +0500

    dbcontext

--- Authentication.WebApi/Program.cs
+++ Authentication.WebApi/Program.cs
@@ -1,8 +1,17 @@
+using Authentication.DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+builder.Services.AddDbContext<AuthenticationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
