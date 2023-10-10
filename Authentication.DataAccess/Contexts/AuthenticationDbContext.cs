97c42c70fcab9681fc0f82a8520df5c527c63664
dotnetsharpist <sheyxcontacts@gmail.com>
2023-10-11 00:57:27 +0500

    Repository

---
+++ Authentication.DataAccess/Contexts/AuthenticationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Authentication.Domain.Entities.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.DataAccess.Contexts;
+
+public class AuthenticationDbContext : DbContext
+{
+    public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) : base(options)
+    { }
+
+    public DbSet<User> users { get; set; }
+}
---
+++ Authentication.DataAccess/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Authentication.Domain.Entities.User;
+
+namespace Authentication.DataAccess.IRepositories;
+
+public interface IUnitOfWork : IDisposable
+{
+    IRepository<User> UserRepository { get; }
+    Task<bool> SaveAsync();
+}
